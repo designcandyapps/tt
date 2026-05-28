f45f12f417dc6941ab477223d5503f20b0c30b4d
Erin Dunn Halligan <erin@designcandy.com>
2026-05-28 16:04:50 -0500

    Refactor input elements in index.vue template

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,8 +34,8 @@ onMounted(()=>{
 <template>
   <ULandingHero v-if="page.hero" v-bind="page.hero">
     <span class="g">
-      <span id="z"><ColorThief /></span><input id="prompt" v-model="prompt">
-      input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
+      <span id="z"><ColorThief /></span><input id="prompt" v-model="prompt"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
+      <input id="pr2" v-model="pr2"><br><input id="pr3" v-model="pr3"><br><input id="pr4" v-model="pr4"><br><input id="pr5" v-model="pr5">
       <span id="response" v-if="response">{{response}}</span>
     </span>
     <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
