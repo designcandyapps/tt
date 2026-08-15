f0bc4350c7bd7a2d4b6b1c0e0d5e92844bd7aaa1
Erin Dunn Halligan <erin@designcandy.com>
2026-08-15 14:25:53 -0500

    Update tt.js

--- server/api/tt.js
+++ server/api/tt.js
@@ -1,5 +1,5 @@
 export default defineEventHandler(async()=>{
-  const response=await fetch("https://api.tickettailor.com/v1/events/:2036131",{
+  const response=await fetch("https://api.tickettailor.com/v1/events",{
     headers:{
       Accept:"application/json",
       Authorization:"Basic "+Buffer.from("sk_14995_133548_95cbe0f619ded70f2d57a144acefffc5:").toString("base64")},
