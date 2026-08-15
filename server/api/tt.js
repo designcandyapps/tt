baa6ecdd7ef625ef17c4664690037d737905db47
Erin Dunn Halligan <erin@designcandy.com>
2026-08-15 14:34:21 -0500

    Refactor event handler for API response fetching

--- server/api/tt.js
+++ server/api/tt.js
@@ -1,11 +1,13 @@
-export default defineEventHandler(async()=>{
-  const response=await fetch("https://api.tickettailor.com/v1/events",{
-    headers:{
-      Accept:"application/json",
-      Authorization:"Basic "+Buffer.from("sk_14995_133548_95cbe0f619ded70f2d57a144acefffc5:").toString("base64")},
+export default defineEventHandler(async () => {
+  const response = await fetch("https://api.tickettailor.com/v1/events", {
+    headers: {
+      Accept: "application/json",
+      Authorization:
+        "Basic " + Buffer.from("sk_14995_133548_95cbe0f619ded70f2d57a144acefffc5:").toString("base64"),
+    },
   });
-  const data=await response.json();
-  alert("Test");
-  //alert("RES1P: "+JSON.stringify(data));
+
+  const data = await response.json();
+
   return data;
 });
